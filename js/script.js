3331c8655b5cce7c85cb1d8b99fad9b7c2fc5a7d
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2025-03-29 21:32:50 +0100

    Added a hamburger menu in the main page.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
     const body = document.body;
+    const menuToggle = document.getElementById('menu-toggle');
+    const menuContent = document.querySelector('.menu-content');
 
     // Function to toggle the theme
     function toggleTheme() {
@@ -17,4 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if (currentTheme === 'dark') {
         body.classList.add('dark-mode');
     }
+
+    // Function to toggle the menu
+    function toggleMenu() {
+        menuContent.classList.toggle('open');
+    }
+
+    // Event listener for the menu toggle button
+    menuToggle.addEventListener('click', toggleMenu);
 });
